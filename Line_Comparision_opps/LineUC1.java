104a033a913e445daf9a611ad25062c2d047753e
Darshan Gowda R <darshangowdar@pop-os.localdomain>
2022-02-06 10:04:24 +0530

    As a fan of geometry, I want to model a line based on a point consisting of (x, y).

---
+++ Line_Comparision_opps/LineUC1.java
@@ -0,0 +1,12 @@
+public class LineUC1{
+
+	public static void main(String args[]){
+	int x1=2,x2=4,y1=3,y2=6;
+
+	double length1=Math.sqrt((x2-x1)^2+(y2-y1)^2);
+	System.out.format("The length of two point : %.2f ",length1);
+	System.out.println("");
+	}
+
+
+}
